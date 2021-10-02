3526482208cccf720a0b0fc094a5073e90dd407d
Elechim01 <68447158+Elechim01@users.noreply.github.com>
2021-10-02 22:02:45 +0200

    Initial Commit

---
+++ Shared/ElegantTaskAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ElegantTaskAppApp.swift
+//  Shared
+//
+//  Created by Michele Manniello on 02/10/21.
+//
+
+import SwiftUI
+
+@main
+struct ElegantTaskAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
